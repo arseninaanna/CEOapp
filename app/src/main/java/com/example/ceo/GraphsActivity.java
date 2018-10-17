88e38882ff9f335b113cbe13a4b2346b72777318
Vlad Kuleykin <kzvdar42@gmail.com>
2018-10-17 20:36:06 +0300

    Added support for the toolbar in `GraphsActivity`.

--- app/src/main/java/com/example/ceo/GraphsActivity.java
+++ app/src/main/java/com/example/ceo/GraphsActivity.java
@@ -1,22 +1,31 @@
 package com.example.ceo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Objects;
+
 public class GraphsActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
 
-        GraphView happiness = (GraphView) findViewById(R.id.happiness);
+        // Add the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.graphs_activity_label);
+        setSupportActionBar(toolbar);
+
+        // Set back button on toolbar
+        toolbar.setNavigationOnClickListener(v -> finish());
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        GraphView happiness = findViewById(R.id.happiness);
         double x[] = {1, 2, 3, 5};
         double y[] = {1, 2, 3, 4};
         LineGraphSeries<DataPoint> series = addSeries(x, y);
@@ -24,22 +33,15 @@ public class GraphsActivity extends AppCompatActivity {
 
     }
 
-    public LineGraphSeries<DataPoint> addSeries(double x[],double y[]){
+    public LineGraphSeries<DataPoint> addSeries(double x[], double y[]) {
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-        for(int i = 0; i < x.length; i++){
+        for (int i = 0; i < x.length; i++) {
             DataPoint point = new DataPoint(x[i], y[i]);
             series.appendData(point, true, 100, false);
 
         }
 
         return series;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
     }
 
     @Override
