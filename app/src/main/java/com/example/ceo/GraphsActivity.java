9d6f9a7b47200558bf51fd598df83acd925e92fa
Vlad Kuleykin <kzvdar42@gmail.com>
2018-10-17 22:39:38 +0300

    Set the style of `GraphActivity` to the app style. Inflated the graphs.

--- app/src/main/java/com/example/ceo/GraphsActivity.java
+++ app/src/main/java/com/example/ceo/GraphsActivity.java
@@ -3,7 +3,6 @@ package com.example.ceo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -25,11 +24,46 @@ public class GraphsActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(v -> finish());
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        // FIXME: Make this more elegant
+        // Adding data to the graphs:
+
         GraphView happiness = findViewById(R.id.happiness);
-        double x[] = {1, 2, 3, 5};
-        double y[] = {1, 2, 3, 4};
-        LineGraphSeries<DataPoint> series = addSeries(x, y);
-        happiness.addSeries(series);
+        happiness.setTitleTextSize(52); // FIXME: move to the values
+
+        // Setting the color to black
+        happiness.getGridLabelRenderer().setGridColor(getColor(R.color.black));
+        happiness.getGridLabelRenderer().setHorizontalLabelsColor(getColor(R.color.black));
+        happiness.getGridLabelRenderer().setVerticalLabelsColor(getColor(R.color.black));
+        happiness.getGridLabelRenderer().setVerticalAxisTitleColor(getColor(R.color.black));
+        happiness.getGridLabelRenderer().setHorizontalAxisTitleColor(getColor(R.color.black));
+        happiness.setTitleColor(getColor(R.color.black));
+
+        // Setting the title
+        happiness.setTitle(getResources().getString(R.string.customer_happiness_label));
+
+        // Adding the data
+        double x1[] = {2014, 2015, 2016, 2017, 2018};
+        double y1[] = {3.4, 3.6, 4.0, 4.1, 4.35};
+        happiness.addSeries(addSeries(x1, y1));
+
+        GraphView orders = findViewById(R.id.orders);
+
+        // Setting the color to black
+        orders.setTitleTextSize(52); // FIXME: move to the values
+        orders.getGridLabelRenderer().setGridColor(getColor(R.color.black));
+        orders.getGridLabelRenderer().setHorizontalLabelsColor(getColor(R.color.black));
+        orders.getGridLabelRenderer().setVerticalLabelsColor(getColor(R.color.black));
+        orders.getGridLabelRenderer().setVerticalAxisTitleColor(getColor(R.color.black));
+        orders.getGridLabelRenderer().setHorizontalAxisTitleColor(getColor(R.color.black));
+        orders.setTitleColor(getColor(R.color.black));
+
+        // Setting the title
+        orders.setTitle(getResources().getString(R.string.orders_number_label));
+
+        // Adding the data
+        double y2[] = {300456, 356523, 420546, 490523, 545622};
+        double x2[] = {2014, 2015, 2016, 2017, 2018};
+        orders.addSeries(addSeries(x2, y2));
 
     }
 
@@ -42,16 +76,5 @@ public class GraphsActivity extends AppCompatActivity {
         }
 
         return series;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.back_arrow:
-                finish();
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
